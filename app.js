c84bb30957a245c8908feaa5c63489a318a5a828
brianmsj <brianmsj@gmail.com>
2017-02-06 08:18:29 -0800

    created HTML draft

---
+++ app.js
@@ -0,0 +1,11 @@
+// We want to create a quiz, with 10 questions. Each question will have 4 multiple choice answers.
+// If we use the state programming message.
+
+
+//create our state object. We will not have any text inputs to this quiz, so we will initially place the objects of the questions and answers in the state object.
+
+var questions = [
+  { question: "How many players on a basketball roster?"
+    Choices: "5", "7"
+}
+]
