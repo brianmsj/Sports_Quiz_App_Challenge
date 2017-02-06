7398cf2204ea38c4d4f46945dc19b7445d6539a3
James Guthrie <jamesrguthrie@gmail.com>
2017-02-06 11:46:30 -0500

    added state, and event listener on start button

--- app.js
+++ app.js
@@ -4,8 +4,35 @@
 
 //create our state object. We will not have any text inputs to this quiz, so we will initially place the objects of the questions and answers in the state object.
 
-var questions = [
-  { question: "How many players on a basketball roster?"
-    Choices: "5", "7"
-}
+var questionObjects = [
+
+  { 
+    question: "How many players on a basketball roster?",
+    Choices: ['5', '7', '12', '10'],
+    answer: "12",
+  },
 ]
+
+
+
+var state = {
+  questions: questionObjects,
+}
+
+
+
+
+
+function initializeEventListeners() {
+
+  $('.start-quiz').click(function() {
+    $('.home-screen').addClass('hidden');
+    $('.multiple-choice').removeClass('hidden');
+  });
+
+}
+
+$(function() {
+  initializeEventListeners();
+
+});
